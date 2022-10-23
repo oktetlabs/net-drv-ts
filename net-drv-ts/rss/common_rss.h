ea177cb415d8d7313cd1c41aac63d79f7d1f5046
Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
2022-10-23 07:58:50 +0300

    rss: extend TAPI, move some code from tests to it
    Move similar code related to Toeplitz hash computation to TAPI.
    Fix hash computation for the case when prologue detected nonstandard
    Toeplitz hash algorithm.
    Remove some checks from tests which now are done by prologue.
    
    OL-Redmine-Id: 12456
    Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

--- net-drv-ts/rss/common_rss.h
+++ net-drv-ts/rss/common_rss.h
@@ -13,6 +13,7 @@
 #include "net_drv_test.h"
 #include "tapi_cfg_if_rss.h"
 #include "tapi_bpf.h"
+#include "te_toeplitz.h"
 
 /**
  * Send a few packets and check whether expected Rx queue received them.
@@ -122,5 +123,83 @@ net_drv_rss_get_check_hkey(const char *ta, const char *if_name,
 
     RING("RSS hash key: %Tm", *key, *len);
 }
+
+/** Helper structure for RSS tests */
+typedef struct net_drv_rss_ctx {
+    const char *ta; /**< Test Agent name */
+    const char *if_name; /**< Interface name */
+    unsigned int rss_ctx; /**< RSS context id */
+    unsigned int indir_table_size; /**< Size of indirection table */
+    unsigned int rx_queues; /**< Number of Rx queues */
+    uint8_t *hash_key; /**< Hash key */
+    size_t key_len; /**< Length of hash key */
+    te_toeplitz_hash_cache *cache; /**< Cache used for computing Toeplitz
+                                        hash */
+    te_toeplitz_hash_variant hash_variant; /**< Variant of Toeplitz
+                                                algorithm to use */
+} net_drv_rss_ctx;
+
+/** Initializer for net_drv_rss_ctx */
+#define NET_DRV_RSS_CTX_INIT \
+    {                                                                     \
+        .ta = "", .if_name = "", .rss_ctx = 0, .indir_table_size = 0,     \
+        .rx_queues = 0, .hash_key = NULL, .key_len = 0, .cache = NULL,    \
+        .hash_variant = TE_TOEPLITZ_HASH_STANDARD                         \
+    }
+
+/**
+ * Fill fields of RSS test context.
+ *
+ * @note This function can print verdict and jump to cleanup.
+ *
+ * @param ctx       Context structure to initialize
+ * @param ta        Test agent name
+ * @param if_name   Interface name
+ * @param rss_ctx   RSS context id
+ */
+extern void net_drv_rss_ctx_prepare(net_drv_rss_ctx *ctx,
+                                    const char *ta,
+                                    const char *if_name,
+                                    unsigned int rss_ctx);
+
+/**
+ * Release resources allocated for RSS test context.
+ *
+ * @param ctx     Pointer to context structure
+ */
+extern void net_drv_rss_ctx_release(net_drv_rss_ctx *ctx);
+
+/**
+ * Change hash key used for Toeplitz hash computation.
+ *
+ * @param ctx       Pointer to context structure
+ * @param hash_key  New hash key
+ * @param key_len   Key length
+ *
+ * @return Status code.
+ */
+extern te_errno net_drv_rss_ctx_change_key(net_drv_rss_ctx *ctx,
+                                           uint8_t *hash_key,
+                                           unsigned int key_len);
+
+/**
+ * Predict Toeplitz hash value, indirection table index and
+ * Rx queue id for given pairs of addresses/ports.
+ *
+ * @param ctx         RSS test context
+ * @param src_addr    Source address/port
+ * @param dst_addr    Destination address/port
+ * @param hash_out    Where to save hash value (may be @c NULL)
+ * @param idx_out     Where to save indirection table index (may be @c NULL)
+ * @param queue_out   Where to save Rx queue id (may be @c NULL)
+ *
+ * @return Status code.
+ */
+extern te_errno net_drv_rss_predict(net_drv_rss_ctx *ctx,
+                                    const struct sockaddr *src_addr,
+                                    const struct sockaddr *dst_addr,
+                                    unsigned int *hash_out,
+                                    unsigned int *idx_out,
+                                    unsigned int *queue_out);
 
 #endif /* !__TS_NET_DRV_COMMON_RSS_H__ */
