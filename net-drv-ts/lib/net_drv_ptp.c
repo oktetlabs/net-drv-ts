oktetlabs/net-drv-ts

commit 9d2abb5fe5a8409cd878c22a2c9d45f02eaae363

ptp: implement a test on synchronizing clocks with ptp4l
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>

net-drv-ts/ptp/ptp4l.c
@@ -1,0 +1,285 @@
+/* SPDX-License-Identifier: Apache-2.0 */
+/* Copyright (C) 2022 OKTET Labs Ltd. All rights reserved. */
+
+/*
+ * Net Driver Test Suite
+ * PTP tests
+ */
+
+/**
+ * @defgroup ptp-ptp4l Use ptp4l to synchronize clocks
+ * @ingroup ptp
+ * @{
+ *
+ * @objective Check that ptp4l can be used to synchronize clocks
+ *            across network.
+ *
+ * @param env              Testing environment:
+ *                         - @ref env-peer2peer
+ *
+ * @par Scenario:
+ */
+
+#define TE_TEST_NAME "ptp/ptp4l"
+
+#include <math.h>
+
+#include "net_drv_test.h"
+#include "tapi_ntpd.h"
+#include "tapi_job.h"
+#include "tapi_job_factory_rpc.h"
+
+/* How long to run ptp4l, in seconds */
+#define RUN_TIME 100
+
+/* How long to wait for ptp4l termination, in ms */
+#define JOB_WAIT_TIMEOUT 100
+
+/* Minimum initial value of tv_sec field */
+#define MIN_SEC 0
+/* Maximum initial value of tv_sec field */
+#define MAX_SEC 1000000000
+/*
+ * Minimum initial difference in timestamps between IUT and Tester, in
+ * seconds
+ */
+#define MIN_DIFF 2
+
+/*
+ * Maximum difference in seconds between IUT and Tester clocks
+ * after running ptp4l.
+ */
+#define MAX_DIFF_AFTER_RUN 0.2
+
+/*
+ * Try to find ptp4l application.
+ * If it is missing, test should be skipped.
+ */
+static void
+find_ptp4l(rcf_rpc_server *rpcs, const char *where)
+{
+    rpc_wait_status st;
+
+    RPC_AWAIT_ERROR(rpcs);
+    st = rpc_system(rpcs, "which ptp4l");
+    if (st.flag != RPC_WAIT_STATUS_EXITED || st.value != 0)
+        TEST_SKIP("Cannot find ptp4l on %s", where);
+}
+
+/* Check messages printed to stderr by ptp4l. */
+static void
+check_errs(tapi_job_channel_t *err_chan)
+{
+    tapi_job_buffer_t *bufs = NULL;
+    unsigned int count = 0;
+    te_errno rc;
+    unsigned int i;
+
+    te_bool tx_timeout = FALSE;
+    te_bool sync_without_ts = FALSE;
+    te_bool unknown = FALSE;
+
+    const char *tx_timeout_str =
+          "timed out while polling for tx timestamp";
+    const char *sync_without_ts_str =
+          "received SYNC without timestamp";
+    const char *msg = NULL;
+
+    rc = tapi_job_receive_many(
+                    TAPI_JOB_CHANNEL_SET(err_chan),
+                    0, &bufs, &count);
+    if (rc != 0)
+    {
+        WARN_VERDICT("tapi_job_receive_many() returned %r "
+                     "when retrieving error messages", rc);
+    }
+
+    for (i = 0; i < count; i++)
+    {
+        if (bufs[i].eos)
+            break;
+
+        msg = te_string_value(&bufs[i].data);
+        if (strstr(msg, tx_timeout_str) != NULL)
+            tx_timeout = TRUE;
+        else if (strstr(msg, sync_without_ts_str) != NULL)
+            sync_without_ts = TRUE;
+        else
+            unknown = TRUE;
+    }
+
+    tapi_job_buffers_free(bufs, count);
+
+    if (tx_timeout)
+        WARN_VERDICT("Error on IUT: %s", tx_timeout_str);
+    if (sync_without_ts)
+        WARN_VERDICT("Error on IUT: %s", sync_without_ts_str);
+    if (unknown)
+        WARN_VERDICT("Unexpected error was logged on IUT");
+}
+
+/* Wait for ptp4l termination, check its exit code. */
+static void
+wait_ptp4l(tapi_job_t *job, const char *where)
+{
+    tapi_job_status_t st;
+
+    CHECK_RC(tapi_job_wait(job, JOB_WAIT_TIMEOUT, &st));
+    if (st.type != TAPI_JOB_STATUS_EXITED)
+    {
+        WARN_VERDICT("ptp4l terminated in unexpected way on %s", where);
+    }
+    else if (st.value != 0)
+    {
+        WARN_VERDICT("ptp4l terminated with nonzero exit code on %s",
+                     where);
+    }
+}
+
+int
+main(int argc, char *argv[])
+{
+    rcf_rpc_server *iut_rpcs = NULL;
+    rcf_rpc_server *tst_rpcs = NULL;
+    const struct if_nameindex *iut_if = NULL;
+    const struct if_nameindex *tst_if = NULL;
+
+    tapi_job_factory_t *iut_factory = NULL;
+    tapi_job_factory_t *tst_factory = NULL;
+    tapi_job_t *master_job = NULL;
+    tapi_job_t *slave_job = NULL;
+    tapi_job_channel_t *master_channels[2] = { NULL, NULL };
+    tapi_job_channel_t *slave_channels[2] = { NULL, NULL };
+    tapi_job_channel_t *slave_err_filter = NULL;
+
+    int fd_iut = -1;
+    int fd_tst = -1;
+    tarpc_timespec ts_iut;
+    tarpc_timespec ts_tst;
+    double diff;
+
+    TEST_START;
+    TEST_GET_PCO(iut_rpcs);
+    TEST_GET_PCO(tst_rpcs);
+    TEST_GET_IF(iut_if);
+    TEST_GET_IF(tst_if);
+
+    TEST_STEP("Find out PTP device associated with the IUT interface, "
+              "call @b open() to get its FD.");
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd_iut);
+
+    TEST_STEP("Find out PTP device associated with the Tester interface, "
+              "call @b open() to get its FD.");
+    net_drv_open_ptp_fd(tst_rpcs, tst_if->if_name, &fd_tst);
+
+    TEST_STEP("Check that @b ptp4l is available on IUT and Tester.");
+    find_ptp4l(iut_rpcs, "IUT");
+    find_ptp4l(tst_rpcs, "Tester");
+
+    TEST_STEP("Disable @b NTP daemon on IUT and Tester.");
+    tapi_ntpd_disable(iut_rpcs);
+    tapi_ntpd_disable(tst_rpcs);
+
+    TEST_STEP("Use @b clock_settime() on IUT and Tester to set different "
+              "initial values for IUT and Tester clocks.");
+
+    while (TRUE)
+    {
+        ts_iut.tv_sec = rand_range(MIN_SEC, MAX_SEC);
+        ts_iut.tv_nsec = rand_range(0, 999999999);
+
+        ts_tst.tv_sec = rand_range(MIN_SEC, MAX_SEC);
+        ts_tst.tv_nsec = rand_range(0, 999999999);
+
+        diff = net_drv_timespec_diff(&ts_iut, &ts_tst);
+        if (fabs(diff) >= MIN_DIFF)
+            break;
+    }
+
+    rpc_clock_settime(iut_rpcs, TARPC_CLOCK_ID_FD, fd_iut, &ts_iut);
+    rpc_clock_settime(tst_rpcs, TARPC_CLOCK_ID_FD, fd_tst, &ts_tst);
+
+    RING("Initial difference between device clocks: %f", diff);
+
+    CHECK_RC(tapi_job_factory_rpc_create(iut_rpcs,
+                                         &iut_factory));
+    CHECK_RC(tapi_job_factory_rpc_create(tst_rpcs,
+                                         &tst_factory));
+
+    CHECK_RC(tapi_job_create(
+               tst_factory, NULL, "ptp4l",
+               (const char *[]){"ptp4l", "-m", "-i", tst_if->if_name,
+               NULL}, NULL, &master_job));
+
+    CHECK_RC(tapi_job_create(
+               iut_factory, NULL, "ptp4l",
+               (const char *[]){"ptp4l", "-m", "-s", "-i", iut_if->if_name,
+               NULL}, NULL, &slave_job));
+
+    CHECK_RC(tapi_job_alloc_output_channels(master_job, 2,
+                                            master_channels));
+    CHECK_RC(tapi_job_alloc_output_channels(slave_job, 2,
+                                            slave_channels));
+
+    CHECK_RC(tapi_job_attach_filter(
+                TAPI_JOB_CHANNEL_SET(master_channels[0]),
+                "Master_stdout", FALSE, TE_LL_RING, NULL));
+
+    CHECK_RC(tapi_job_attach_filter(
+                TAPI_JOB_CHANNEL_SET(master_channels[1]),
+                "Master_stderr", FALSE, TE_LL_WARN, NULL));
+
+    CHECK_RC(tapi_job_attach_filter(
+                TAPI_JOB_CHANNEL_SET(slave_channels[0]),
+                "Slave_stdout", FALSE, TE_LL_RING, NULL));
+
+    CHECK_RC(tapi_job_attach_filter(
+                TAPI_JOB_CHANNEL_SET(slave_channels[1]),
+                "Slave_stderr", TRUE, TE_LL_WARN, &slave_err_filter));
+
+    TEST_STEP("Run @b ptp4l as master on Tester.");
+    CHECK_RC(tapi_job_start(master_job));
+
+    TEST_STEP("Run @b ptp4l as slave on IUT.");
+    CHECK_RC(tapi_job_start(slave_job));
+
+    TEST_STEP("Wait for a while and terminate @b ptp4l on IUT and Tester.");
+    SLEEP(RUN_TIME);
+
+    CHECK_RC(tapi_job_kill(master_job, RPC_SIGINT));
+    CHECK_RC(tapi_job_kill(slave_job, RPC_SIGINT));
+
+    wait_ptp4l(master_job, "Tester");
+    wait_ptp4l(slave_job, "IUT");
+
+    TEST_STEP("Check whether anything was printed to stderr on IUT.");
+    check_errs(slave_err_filter);
+
+    TEST_STEP("Obtain current time on the IUT clock and on the Tester "
+              "clock, check that obtained values are (approximately) "
+              "equal.");
+
+    rpc_clock_gettime(iut_rpcs, TARPC_CLOCK_ID_FD, fd_iut, &ts_iut);
+    rpc_clock_gettime(tst_rpcs, TARPC_CLOCK_ID_FD, fd_tst, &ts_tst);
+    diff = net_drv_timespec_diff(&ts_iut, &ts_tst);
+
+    RING("Final difference between device clocks: %f",
+         diff);
+
+    if (fabs(diff) > MAX_DIFF_AFTER_RUN)
+        TEST_VERDICT("ptp4l did not synchronize clocks well");
+
+    TEST_SUCCESS;
+
+cleanup:
+
+    CLEANUP_RPC_CLOSE(iut_rpcs, fd_iut);
+    CLEANUP_RPC_CLOSE(tst_rpcs, fd_tst);
+
+    CLEANUP_CHECK_RC(tapi_job_destroy(master_job, 0));
+    CLEANUP_CHECK_RC(tapi_job_destroy(slave_job, 0));
+    tapi_job_factory_destroy(iut_factory);
+    tapi_job_factory_destroy(tst_factory);
+
+    TEST_END;
+}

trc/ptp.xml
@@ -98,5 +98,13 @@
         <notes/>
       </iter>
     </test>
+    <test name="ptp4l" type="script">
+      <objective>Check that ptp4l can be used to synchronize clocks across network.</objective>
+      <notes/>
+      <iter result="PASSED">
+        <arg name="env"/>
+        <notes/>
+      </iter>
+    </test>
   </iter>
 </test>

net-drv-ts/ptp/meson.build
@@ -7,6 +7,7 @@ tests = [
     'clock_caps',
     'get_time',
     'set_time',
+    'ptp4l',
     'sfptpd',
     'sys_offset',
     'sys_offset_extended',

net-drv-ts/ptp/package.xml
@@ -142,5 +142,12 @@
             </arg>
         </run>
 
+        <run>
+            <script name="ptp4l"/>
+            <arg name="env">
+                <value ref="env.peer2peer"/>
+            </arg>
+        </run>
+
     </session>
 </package>

commit 3ceb782bb73f3d6f5f2d5b710b70b817019e94fd

ptp: make verdicts different for different hosts
It is better when verdict tells whether problem with opening PTP device
occurred on IUT or on Tester.

Fixes: 9d2abb5fe5a8 ("ptp: implement a test on synchronizing clocks with ptp4l")
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

net-drv-ts/lib/net_drv_ptp.c
@@ -13,14 +13,26 @@
 
 /* See description in net_drv_ptp.h */
 void
-net_drv_open_ptp_fd(rcf_rpc_server *rpcs, const char *if_name, int *fd)
+net_drv_open_ptp_fd(rcf_rpc_server *rpcs, const char *if_name, int *fd,
+                    const char *vpref)
 {
     struct ifreq ifreq_var;
     struct tarpc_ethtool_ts_info ts_info;
 
     char path[1024];
+    te_string vpref_str = TE_STRING_INIT_STATIC(256);
     int s = -1;
     int rc;
+
+    if (vpref != NULL && *vpref != '\0')
+    {
+        te_string_append(&vpref_str, "%s: ", vpref);
+        vpref = te_string_value(&vpref_str);
+    }
+    else
+    {
+        vpref = "";
+    }
 
     s = rpc_socket(rpcs, RPC_PF_INET, RPC_SOCK_DGRAM, RPC_PROTO_DEF);
 
@@ -36,23 +48,23 @@ net_drv_open_ptp_fd(rcf_rpc_server *rpcs, const char *if_name, int *fd)
     rc = rpc_ioctl(rpcs, s, RPC_SIOCETHTOOL, &ifreq_var);
     if (rc < 0)
     {
-        ERROR_VERDICT("ioctl(SIOCETHTOOL/ETHTOOL_GET_TS_INFO) failed with "
-                      "error %r", RPC_ERRNO(rpcs));
+        ERROR_VERDICT("%sioctl(SIOCETHTOOL/ETHTOOL_GET_TS_INFO) failed "
+                      "with error %r", vpref, RPC_ERRNO(rpcs));
         RPC_CLOSE(rpcs, s);
         TEST_STOP;
     }
     RPC_CLOSE(rpcs, s);
 
     if (ts_info.phc_index < 0)
-        TEST_SKIP("PTP device index is not known");
+        TEST_SKIP("%sPTP device index is not known", vpref);
 
     TE_SPRINTF(path, "/dev/ptp%d", (int)(ts_info.phc_index));
     RPC_AWAIT_ERROR(rpcs);
     *fd = rpc_open(rpcs, path, RPC_O_RDWR, 0);
     if (*fd < 0)
     {
-        TEST_VERDICT("Failed to open PTP device file, errno=%r",
-                     RPC_ERRNO(rpcs));
+        TEST_VERDICT("%sfailed to open PTP device file, errno=%r",
+                     vpref, RPC_ERRNO(rpcs));
     }
 }
 

net-drv-ts/lib/net_drv_ptp.h
@@ -42,9 +42,10 @@
  * @param rpcs      RPC server
  * @param if_name   Interface name
  * @param fd        Where to save opened file descriptor
+ * @param vpref     Prefix for verdicts (may be @c NULL or empty)
  */
 extern void net_drv_open_ptp_fd(rcf_rpc_server *rpcs, const char *if_name,
-                                int *fd);
+                                int *fd, const char *vpref);
 
 /**
  * Get difference between two timespec structures in seconds

net-drv-ts/ptp/adj_frequency.c
@@ -125,7 +125,7 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
 
     TEST_STEP("Call @b clock_adjtime() to check whether current frequency "
               "offset is zero.");

net-drv-ts/ptp/adj_setoffset.c
@@ -151,7 +151,7 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
 
     TEST_STEP("Use @b clock_adjtime() to add @p time_offset seconds "
               "to the current time on the PTP device. With help of "

net-drv-ts/ptp/clock_caps.c
@@ -46,7 +46,7 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
 
     TEST_STEP("Call @b ioctl(@c PTP_CLOCK_GETCAPS) on the PTP device FD.");
     RPC_AWAIT_ERROR(iut_rpcs);

net-drv-ts/ptp/get_time.c
@@ -54,7 +54,7 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
 
     TEST_STEP("Call @b clock_gettime() with PTP device FD to get the "
               "first timestamp.");

net-drv-ts/ptp/ptp4l.c
@@ -166,11 +166,11 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd_iut);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd_iut, "IUT");
 
     TEST_STEP("Find out PTP device associated with the Tester interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(tst_rpcs, tst_if->if_name, &fd_tst);
+    net_drv_open_ptp_fd(tst_rpcs, tst_if->if_name, &fd_tst, "Tester");
 
     TEST_STEP("Check that @b ptp4l is available on IUT and Tester.");
     find_ptp4l(iut_rpcs, "IUT");

net-drv-ts/ptp/set_time.c
@@ -62,7 +62,7 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
 
     if (ts < 0)
     {

net-drv-ts/ptp/sfptpd.c
@@ -281,7 +281,7 @@ main(int argc, char *argv[])
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "check that it can be opened to be sure that PTP is "
               "supported.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
     RPC_CLOSE(iut_rpcs, fd);
 
     CHECK_RC(tapi_job_factory_rpc_create(iut_rpcs, &factory));

net-drv-ts/ptp/sys_offset.c
@@ -53,7 +53,7 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
 
     TEST_STEP("Call @b ioctl(@c PTP_SYS_OFFSET) on the PTP device FD "
               "asking for @p n_samples samples.");

net-drv-ts/ptp/sys_offset_extended.c
@@ -50,7 +50,7 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
 
     TEST_STEP("Call @b ioctl(@c PTP_SYS_OFFSET_EXTENDED) on the PTP device "
               "FD asking for @p n_samples samples.");

net-drv-ts/ptp/sys_offset_precise.c
@@ -42,7 +42,7 @@ main(int argc, char *argv[])
 
     TEST_STEP("Find out PTP device associated with the IUT interface, "
               "call @b open() to get its FD.");
-    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd);
+    net_drv_open_ptp_fd(iut_rpcs, iut_if->if_name, &fd, "");
 
     TEST_STEP("Call @b ioctl(@c PTP_SYS_OFFSET_PRECISE) on the PTP device "
               "FD to get timestamps for PTP device, @c CLOCK_REALTIME "

trc/ptp.xml
@@ -228,12 +228,12 @@
         <notes/>
         <results tags="ice&amp;linux-mm&lt;514" key="WONTFIX PTP">
           <result value="SKIPPED">
-            <verdict>PTP device index is not known</verdict>
+            <verdict>IUT: PTP device index is not known</verdict>
           </result>
         </results>
         <results tags="virtio-pci" key="NO-PTP" notes="Virtio does not support PTP">
           <result value="SKIPPED">
-            <verdict>PTP device index is not known</verdict>
+            <verdict>IUT: PTP device index is not known</verdict>
           </result>
         </results>
       </iter>