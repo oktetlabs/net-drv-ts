oktetlabs/net-drv-ts

commit c6af6c7eef7b2ca552fd0e386304d3fae5c3f0bc

ts: fix net_drv_sendto_recv_check() function
Sometimes receive function on peer host receives only patch of packet
on the first call so it should be called more times until all data were
received

AMD-Jira-Id: NET-69
Signed-off-by: Yurij Plotnikov <Yurij.Plotnikov@arknetworks.am>

net-drv-ts/lib/net_drv_ts.c
@@ -249,6 +249,7 @@ net_drv_sendto_recv_check(rcf_rpc_server *rpcs_sender,
     te_bool readable;
     int len;
     int rc;
+    int total_len = 0;
 
     if (vpref == NULL || *vpref == '\0')
         vpref = "Data transmission check";
@@ -281,14 +282,23 @@ net_drv_sendto_recv_check(rcf_rpc_server *rpcs_sender,
     if (!readable)
         TEST_VERDICT("%s: receiver did not become readable", vpref);
 
-    RPC_AWAIT_ERROR(rpcs_receiver);
-    rc = rpc_recv(rpcs_receiver, s_receiver, recv_buf, sizeof(recv_buf), 0);
-    if (rc < 0)
+    while (readable)
     {
-        TEST_VERDICT("%s: recv() failed with error " RPC_ERROR_FMT,
-                     vpref, RPC_ERROR_ARGS(rpcs_receiver));
+        RPC_AWAIT_ERROR(rpcs_receiver);
+        rc = rpc_recv(rpcs_receiver, s_receiver, &recv_buf[total_len],
+                      sizeof(recv_buf) - total_len, 0);
+        if (rc < 0)
+        {
+            TEST_VERDICT("%s: recv() failed with error " RPC_ERROR_FMT,
+                         vpref, RPC_ERROR_ARGS(rpcs_receiver));
+        }
+        total_len += rc;
+        if (total_len >= MAX_PKT_LEN)
+            break;
+        RPC_GET_READABILITY(readable, rpcs_receiver, s_receiver,
+                            TAPI_WAIT_NETWORK_DELAY);
     }
-    else if (rc != len)
+    if (total_len != len)
     {
         TEST_VERDICT("%s: recv() returned unexpected result", vpref);
     }

commit 0592aa81569342664cf004df54b3c31fdb16b3f0

lib: avoid unnecessary waiting when receiving data
Some tests send and receive a lot of packets, so it is better to avoid
waiting for more data after expected number of bytes was received in
net_drv_sendto_recv_check_gen(). Every such waiting takes 0.5 seconds,
it slows testing down significantly.

Fixes: c6af6c7eef7b ("ts: fix net_drv_sendto_recv_check() function")
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

net-drv-ts/lib/net_drv_ts.c
@@ -311,7 +311,7 @@ net_drv_sendto_recv_check_gen(rcf_rpc_server *rpcs_sender,
                          vpref, RPC_ERROR_ARGS(rpcs_receiver));
         }
         total_len += rc;
-        if (total_len >= MAX_PKT_LEN)
+        if (total_len >= len)
             break;
         RPC_GET_READABILITY(readable, rpcs_receiver, s_receiver,
                             TAPI_WAIT_NETWORK_DELAY);