oktetlabs/net-drv-ts

commit be36300ee5de10878196dfab6ca710b282013fbc

ts: determine NIC-specific TRC tags from prologue
Currently used script from ts-conf can hang indefinitely and break
testing, avoid using it. Some vendor-specific tags will disappear as
a result, however they are not used in TRC DB anyway currently.

OL-Redmine-ID: 11900
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

net-drv-ts/prologue.c
@@ -20,6 +20,10 @@
 #include "tapi_cfg_modules.h"
 #include "tapi_cfg_pci.h"
 #include "tapi_sh_env.h"
+#include "tapi_cfg_pci.h"
+#include "tapi_tags.h"
+
+#define DEF_STR_LEN 1024
 
 static te_errno
 load_required_modules(const char *ta, void *cookie)
@@ -264,9 +268,84 @@ net_drv_cfg_net_nodes_update_pci_fn_to_interface(enum net_node_type type)
     return rc;
 }
 
+#define FUNC_CHECK_RC(_expr) \
+    do {                            \
+        rc = (_expr);               \
+                                    \
+        if (rc != 0)                \
+            goto finish;            \
+    } while (0)
+
+/* Add TRC tags related to tested NIC */
+static te_errno
+add_nic_tags(const char *ta,
+             const struct if_nameindex *intf)
+{
+    char *pci_oid = NULL;
+    unsigned int vendor_id = 0;
+    unsigned int device_id = 0;
+    unsigned int sub_vendor_id = 0;
+    unsigned int sub_device_id = 0;
+    te_string str = TE_STRING_INIT_STATIC(DEF_STR_LEN);
+    te_errno rc;
+
+    rc = tapi_cfg_pci_oid_by_net_if(ta, intf->if_name, &pci_oid);
+    if (TE_RC_GET_ERROR(rc) == TE_ENOENT)
+        return 0;
+    else if (rc != 0)
+        return rc;
+
+    FUNC_CHECK_RC(tapi_cfg_pci_get_vendor_dev_ids(
+                        pci_oid, &vendor_id, &device_id,
+                        &sub_vendor_id,
+                        &sub_device_id));
+
+    FUNC_CHECK_RC(te_string_append(&str, "pci-%04x", vendor_id));
+    FUNC_CHECK_RC(tapi_tags_add_tag(te_string_value(&str), NULL));
+
+    FUNC_CHECK_RC(te_string_append(&str, "-%04x", device_id));
+    FUNC_CHECK_RC(tapi_tags_add_tag(te_string_value(&str), NULL));
+
+    te_string_reset(&str);
+    FUNC_CHECK_RC(te_string_append(&str, "pci-sub-%04x", sub_vendor_id));
+    FUNC_CHECK_RC(tapi_tags_add_tag(te_string_value(&str), NULL));
+
+    FUNC_CHECK_RC(te_string_append(&str, "-%04x", sub_device_id));
+    FUNC_CHECK_RC(tapi_tags_add_tag(te_string_value(&str), NULL));
+
+finish:
+
+    return rc;
+}
+
+/* Add TRC tag named after network interface driver */
+static te_errno
+add_driver_tag(const char *ta, const char *prefix)
+{
+    char *drv_name = NULL;
+    te_string str = TE_STRING_INIT_STATIC(DEF_STR_LEN);
+    te_errno rc;
+
+    rc = tapi_cfg_pci_get_ta_driver(ta, NET_DRIVER_TYPE_NET, &drv_name);
+    if (rc != 0)
+        return rc;
+
+    rc = te_string_append(&str, "%s%s", prefix, drv_name);
+    if (rc == 0)
+        rc = tapi_tags_add_tag(te_string_value(&str), NULL);
+
+    free(drv_name);
+    return rc;
+}
+
 int
 main(int argc, char **argv)
 {
+    const struct if_nameindex *iut_if = NULL;
+    rcf_rpc_server *iut_rpcs = NULL;
+    rcf_rpc_server *tst_rpcs = NULL;
+    te_bool env_init = FALSE;
+
 /* Redefine as empty to avoid environment processing here */
 #undef TEST_START_VARS
 #define TEST_START_VARS
@@ -323,9 +402,24 @@ main(int argc, char **argv)
     CHECK_RC(rc = cfg_synchronize("/:", TRUE));
     CHECK_RC(rc = cfg_tree_print(NULL, TE_LL_RING, "/:"));
 
+    tapi_env_init(&env);
+    env_init = TRUE;
+    TEST_START_ENV;
+
+    TEST_GET_IF(iut_if);
+    TEST_GET_PCO(iut_rpcs);
+    TEST_GET_PCO(tst_rpcs);
+
+    CHECK_RC(add_driver_tag(iut_rpcs->ta, ""));
+    CHECK_RC(add_driver_tag(tst_rpcs->ta, "peer-"));
+    CHECK_RC(add_nic_tags(iut_rpcs->ta, iut_if));
+
     TEST_SUCCESS;
 
 cleanup:
+
+    if (env_init)
+        TEST_END_ENV;
 
     TEST_END;
 }

conf/opts.ts
@@ -5,5 +5,4 @@
 --script=scripts/defaults
 --script=scripts/disable_unused_agts
 --script=scripts/export_driver_sources
---script=scripts/nic-trc-tags
 --script=scripts/check_bpf

commit 101d445aeedb0efbf0db879d1031d2519ec6aed0

prologue: do not add null driver tag
If driver name is not known, do not try to add meaningless tag with
(null) in prologue.

Fixes: be36300ee5de ("ts: determine NIC-specific TRC tags from prologue")
OL-Redmine-Id: 12830
Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

net-drv-ts/prologue.c
@@ -332,9 +332,12 @@ add_driver_tag(const char *ta, const char *prefix)
     if (rc != 0)
         return rc;
 
-    rc = te_string_append(&str, "%s%s", prefix, drv_name);
-    if (rc == 0)
-        rc = tapi_tags_add_tag(te_string_value(&str), NULL);
+    if (!te_str_is_null_or_empty(drv_name))
+    {
+        rc = te_string_append(&str, "%s%s", prefix, drv_name);
+        if (rc == 0)
+            rc = tapi_tags_add_tag(te_string_value(&str), NULL);
+    }
 
     free(drv_name);
     return rc;