876dbfdf38371405bd65aecaac33839a8649d54e
Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
2025-12-15 17:30:15 +0300

    rx_path/rx_coalesce_usecs: check per queue coalescing
    Check configuring interrupt coalescing for a specific interface queue.
    Make the test less likely to fail due to timing issues.
    
    OL-Redmine-Id: 14675
    Signed-off-by: Dmitry Izbitsky <dmitry.izbitsky@oktetlabs.ru>
    Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

--- net-drv-ts/rx_path/rx_coalesce_usecs.c
+++ net-drv-ts/rx_path/rx_coalesce_usecs.c
@@ -20,6 +20,8 @@
  *                        - @c 0 (interrupt moderation is disabled)
  *                        - @c 300
  *                        - @c 800
+ * @param per_queue       If TRUE, test per queue interrupt coalescing
+ *                        configuration.
  *
  * @par Scenario:
  *
@@ -32,6 +34,8 @@
 #include "tapi_eth.h"
 #include "tapi_udp.h"
 #include "tapi_cfg_if_coalesce.h"
+#include "tapi_cfg_rx_rule.h"
+#include "tapi_cfg_if_chan.h"
 #include "tapi_mem.h"
 
 #include <math.h>
@@ -41,13 +45,18 @@
  * Minimum delay between packets after which the next packet
  * is considered to be from another packets group, microseconds.
  */
-#define MIN_GROUP_DELAY 100
+static unsigned int min_group_delay = 100;
+
+/**
+ * Minimum value for min_group_delay, microseconds.
+ */
+#define MIN_GROUP_DELAY_LOW 30
 
 /**
  * Minimum nonzero value of rx_coalesce_usecs which this test can
  * check reliably.
  */
-#define MIN_NONZERO_USECS (MIN_GROUP_DELAY * 3)
+#define MIN_NONZERO_USECS (MIN_GROUP_DELAY_LOW * 10)
 
 /**
  * Maximum relative deviation of actual packet distrubition
@@ -322,7 +331,7 @@ process_rx_pkts(asn_value *pkt, void *user_data)
     }
     else
     {
-        if (TIMEVAL_SUB(ts, args->prev_ts) > MIN_GROUP_DELAY)
+        if (TIMEVAL_SUB(ts, args->prev_ts) > min_group_delay)
             finish_pkt_group(args, &ts);
 
         args->prev_ts = ts;
@@ -369,6 +378,7 @@ main(int argc, char *argv[])
     int iut_s = -1;
     int tst_s = -1;
 
+    bool per_queue = false;
     int coalesce_usecs;
     int max_frames;
     int send_delay;
@@ -376,13 +386,22 @@ main(int argc, char *argv[])
     uint64_t prev_rx_usecs;
     uint64_t prev_rx_max_frames;
     uint64_t prev_adaptive_rx;
-    te_bool restore_prev_values = FALSE;
+    bool restore_prev_values = false;
+    bool remove_rule = false;
 
     csap_handle_t csap_rx = CSAP_INVALID_HANDLE;
     csap_handle_t csap_tx = CSAP_INVALID_HANDLE;
     tapi_tad_trrecv_cb_data csap_cb_data;
     test_cb_args cb_args = TEST_CB_ARGS_INIT;
     int rc_aux;
+
+    tapi_cfg_rx_rule_flow flow_type;
+    int64_t location = TAPI_CFG_RX_RULE_ANY;
+    int queue_id = -1;
+    int combined_num = 0;
+    int rx_num = 0;
+    int tx_num = 0;
+    int queues_num = 0;
 
     TEST_START;
     TEST_GET_PCO(iut_rpcs);
@@ -392,6 +411,57 @@ main(int argc, char *argv[])
     TEST_GET_ADDR(iut_rpcs, iut_addr);
     TEST_GET_ADDR(tst_rpcs, tst_addr);
     TEST_GET_INT_PARAM(coalesce_usecs);
+    TEST_GET_BOOL_PARAM(per_queue);
+
+    if (per_queue)
+    {
+        CHECK_RC(tapi_cfg_if_chan_cur_get(iut_rpcs->ta, iut_if->if_name,
+                                          TAPI_CFG_IF_CHAN_COMBINED,
+                                          &combined_num));
+        CHECK_RC(tapi_cfg_if_chan_cur_get(iut_rpcs->ta, iut_if->if_name,
+                                          TAPI_CFG_IF_CHAN_RX,
+                                          &rx_num));
+        CHECK_RC(tapi_cfg_if_chan_cur_get(iut_rpcs->ta, iut_if->if_name,
+                                          TAPI_CFG_IF_CHAN_TX,
+                                          &tx_num));
+        queues_num = MAX(rx_num, tx_num) + combined_num;
+        if (queues_num == 0)
+            TEST_SKIP("No interface queues");
+
+        queue_id = rand_range(0, queues_num - 1);
+        RING("Testing queue %d", queue_id);
+
+        TEST_STEP("If @p per_queue is TRUE, configure a rule to use a "
+                  "specific queue to process tested traffic. Interrupt "
+                  "coalescing then is configured for this specific queue "
+                  "rather than for the whole interface.");
+
+        flow_type = tapi_cfg_rx_rule_flow_by_socket(iut_addr->sa_family,
+                                                    RPC_SOCK_DGRAM);
+
+        CHECK_RC(tapi_cfg_rx_rule_find_location(iut_rpcs->ta, iut_if->if_name,
+                                                0, 0, &location));
+
+        CHECK_RC(tapi_cfg_rx_rule_add(iut_rpcs->ta, iut_if->if_name,
+                                      location, flow_type));
+
+        CHECK_RC(tapi_cfg_rx_rule_fill_ip_addrs_ports(
+                                           iut_rpcs->ta, iut_if->if_name,
+                                           location,
+                                           iut_addr->sa_family,
+                                           tst_addr, NULL,
+                                           iut_addr, NULL));
+
+        CHECK_RC(tapi_cfg_rx_rule_rx_queue_set(iut_rpcs->ta, iut_if->if_name,
+                                               location, queue_id));
+
+        rc = tapi_cfg_rx_rule_commit(iut_rpcs->ta, iut_if->if_name,
+                                     location);
+        if (rc != 0)
+            TEST_VERDICT("Failed to add Rx rule: %r", rc);
+
+        remove_rule = true;
+    }
 
     if (coalesce_usecs > 0 &&
         coalesce_usecs < MIN_NONZERO_USECS)
@@ -399,6 +469,9 @@ main(int argc, char *argv[])
         TEST_FAIL("Too small value of coalesce_usecs is requested for "
                   "this test to work");
     }
+
+    min_group_delay = MIN(min_group_delay, coalesce_usecs / 10);
+    min_group_delay = MAX(min_group_delay, MIN_GROUP_DELAY_LOW);
 
     send_delay = MAX(MIN_SEND_DELAY, coalesce_usecs / MAX_COALESCED_PKTS);
     RING("%u us delay between packets is chosen", send_delay);
@@ -412,22 +485,24 @@ main(int argc, char *argv[])
     else
         max_frames = 0;
 
-    rc = tapi_cfg_if_coalesce_get(iut_rpcs->ta, iut_if->if_name,
-                                  "rx_coalesce_usecs",
-                                  &prev_rx_usecs);
+    rc = tapi_cfg_if_coalesce_queue_get(iut_rpcs->ta, iut_if->if_name,
+                                        queue_id, "rx_coalesce_usecs",
+                                        &prev_rx_usecs);
     if (TE_RC_GET_ERROR(rc) == TE_ENOENT)
         TEST_SKIP("Interrupt coalescing settings are not supported");
     else if (rc != 0)
         TEST_FAIL("Failed to get rx_coalesce_usecs: %r", rc);
 
-    CHECK_RC(tapi_cfg_if_coalesce_get(iut_rpcs->ta, iut_if->if_name,
-                                      "rx_max_coalesced_frames",
-                                      &prev_rx_max_frames));
-    CHECK_RC(tapi_cfg_if_coalesce_get(iut_rpcs->ta, iut_if->if_name,
-                                      "use_adaptive_rx_coalesce",
-                                      &prev_adaptive_rx));
+    CHECK_RC(tapi_cfg_if_coalesce_queue_get(iut_rpcs->ta, iut_if->if_name,
+                                            queue_id,
+                                            "rx_max_coalesced_frames",
+                                            &prev_rx_max_frames));
+    CHECK_RC(tapi_cfg_if_coalesce_queue_get(iut_rpcs->ta, iut_if->if_name,
+                                            queue_id,
+                                            "use_adaptive_rx_coalesce",
+                                            &prev_adaptive_rx));
 
-    restore_prev_values = TRUE;
+    restore_prev_values = true;
 
     TEST_STEP("Establish connection between a pair of UDP sockets "
               "on IUT and Tester.");
@@ -441,33 +516,36 @@ main(int argc, char *argv[])
 
     TEST_STEP("Disable @b use_adaptive_rx_coalesce on IUT interface.");
 
-    rc = tapi_cfg_if_coalesce_set(iut_rpcs->ta, iut_if->if_name,
-                                  "use_adaptive_rx_coalesce", 0);
+    rc = tapi_cfg_if_coalesce_queue_set(iut_rpcs->ta, iut_if->if_name,
+                                        queue_id,
+                                        "use_adaptive_rx_coalesce", 0);
     if (rc != 0)
         TEST_VERDICT("Failed to set use_adaptive_rx_coalesce, rc=%r", rc);
 
     TEST_STEP("Set @b rx_coalesce_usecs to the value of @p coalesce_usecs "
-              "for the IUT interface. Also for nonzero @p coalesce_usecs "
-              "set @b rx_max_coalesced_frames to zero so that it is not "
-              "taken into account when coalescing; for zero "
-              "@p coalesce_usecs set it to @c 1.");
+              "for the IUT interface or its queue. Also for nonzero "
+              "@p coalesce_usecs set @b rx_max_coalesced_frames to zero "
+              "so that it is not taken into account when coalescing; for "
+              "zero @p coalesce_usecs set it to @c 1.");
 
-    CHECK_RC(tapi_cfg_if_coalesce_set_local(iut_rpcs->ta, iut_if->if_name,
-                                            "rx_coalesce_usecs",
-                                            coalesce_usecs));
-    CHECK_RC(tapi_cfg_if_coalesce_set_local(iut_rpcs->ta, iut_if->if_name,
-                                            "rx_max_coalesced_frames",
-                                            max_frames));
+    CHECK_RC(tapi_cfg_if_coalesce_queue_set_local(iut_rpcs->ta, iut_if->if_name,
+                                                  queue_id, "rx_coalesce_usecs",
+                                                  coalesce_usecs));
+    CHECK_RC(tapi_cfg_if_coalesce_queue_set_local(iut_rpcs->ta, iut_if->if_name,
+                                                  queue_id,
+                                                  "rx_max_coalesced_frames",
+                                                  max_frames));
 
-    rc = tapi_cfg_if_coalesce_commit(iut_rpcs->ta, iut_if->if_name);
+    rc = tapi_cfg_if_coalesce_queues_commit(iut_rpcs->ta, iut_if->if_name,
+                                            queue_id >= 0);
     if (TE_RC_GET_ERROR(rc) == TE_EOPNOTSUPP)
     {
         TEST_SUBSTEP("If setting fails with @c EOPNOTSUPP, try to set "
                      "only @b rx_coalesce_usecs, as setting "
                      "@b rx_max_coalesced_frames may be not available.");
-        rc = tapi_cfg_if_coalesce_set(iut_rpcs->ta, iut_if->if_name,
-                                      "rx_coalesce_usecs",
-                                      coalesce_usecs);
+        rc = tapi_cfg_if_coalesce_queue_set(iut_rpcs->ta, iut_if->if_name,
+                                            queue_id, "rx_coalesce_usecs",
+                                            coalesce_usecs);
         if (TE_RC_GET_ERROR(rc) == TE_EOPNOTSUPP)
         {
             TEST_SKIP("rx_coalesce_usecs=%u is not supported",
@@ -635,22 +713,35 @@ cleanup:
 
     if (restore_prev_values)
     {
-        CLEANUP_CHECK_RC(tapi_cfg_if_coalesce_set_local(iut_rpcs->ta,
+        CLEANUP_CHECK_RC(tapi_cfg_if_coalesce_queue_set_local(
+                                                        iut_rpcs->ta,
                                                         iut_if->if_name,
+                                                        queue_id,
                                                         "rx_coalesce_usecs",
                                                         prev_rx_usecs));
-        CLEANUP_CHECK_RC(tapi_cfg_if_coalesce_set_local(
+        CLEANUP_CHECK_RC(tapi_cfg_if_coalesce_queue_set_local(
                                               iut_rpcs->ta, iut_if->if_name,
+                                              queue_id,
                                               "rx_max_coalesced_frames",
                                               prev_rx_max_frames));
-        CLEANUP_CHECK_RC(tapi_cfg_if_coalesce_commit(iut_rpcs->ta,
-                                                     iut_if->if_name));
 
-        CLEANUP_CHECK_RC(tapi_cfg_if_coalesce_set(
+        CLEANUP_CHECK_RC(tapi_cfg_if_coalesce_queues_commit(
+                                                    iut_rpcs->ta,
+                                                    iut_if->if_name,
+                                                    queue_id >= 0));
+
+        CLEANUP_CHECK_RC(tapi_cfg_if_coalesce_queue_set(
                                               iut_rpcs->ta,
                                               iut_if->if_name,
+                                              queue_id,
                                               "use_adaptive_rx_coalesce",
                                               prev_adaptive_rx));
+    }
+
+    if (remove_rule)
+    {
+        CLEANUP_CHECK_RC(tapi_cfg_rx_rule_del(iut_rpcs->ta, iut_if->if_name,
+                                              location));
     }
 
     te_vec_free(&cb_args.delayed_pkt_ids);
