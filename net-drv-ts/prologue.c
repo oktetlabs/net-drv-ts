oktetlabs/net-drv-ts

commit 5fd994e842a8b5319065e13968fe91297ef45399

prologue: fix random failures because of unexpected packets
Intel X710 has FW LLDP agent enabled by default. The agent sends LLDP
packets periodically and picky tests fail bacause of unexpected tets.
Disable the agent to avoid these packets.

Signed-off-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Daniil Byshenko <daniil.byshenko@oktetlabs.ru>

net-drv-ts/prologue.c
@@ -21,6 +21,7 @@
 #include "net_drv_test.h"
 
 #include "tapi_cfg_base.h"
+#include "tapi_cfg_if.h"
 #include "tapi_cfg_if_chan.h"
 #include "tapi_cfg_if_rss.h"
 #include "tapi_cfg_net.h"
@@ -99,6 +100,29 @@ cleanup:
     free(driver);
     free(net_driver);
     return rc;
+}
+
+static te_errno
+disable_fw_lldp(cfg_net_t *, cfg_net_node_t *, const char *,
+                cfg_oid *oid, void *)
+{
+    const char *agent = CFG_OID_GET_INST_NAME(oid, 1);
+    const char *iface = CFG_OID_GET_INST_NAME(oid, 2);
+    te_errno rc;
+
+    /*
+     * Try to disable FW LLDP using private flag if it is present.
+     * FW LLDP generates packets from Intel X710 which randomly break
+     * tests because of unexpected packets observed.
+     */
+    rc = tapi_cfg_if_priv_flag_set(agent, iface, "disable-fw-lldp", true);
+    if (rc != 0 && rc != TE_RC(TE_CS, TE_ENOENT))
+    {
+        TEST_VERDICT("Attempt to disable FW LLDP on TA %s interface %s using private flag failed unexpectedly: %r",
+                     agent, iface, rc);
+    }
+
+    return 0;
 }
 
 /* Add TRC tag named after network interface driver */
@@ -258,6 +282,8 @@ main(int argc, char **argv)
     CHECK_RC(tapi_cfg_net_all_assign_ip(AF_INET));
     TEST_STEP("Allocate and assign IPv6 addresses to be used by tests.");
     CHECK_RC(tapi_cfg_net_all_assign_ip(AF_INET6));
+    TEST_STEP("Ensure that FW LLDP is disabled if applicable.");
+    CHECK_RC(tapi_cfg_net_foreach_node(disable_fw_lldp, NULL));
     CFG_WAIT_CHANGES;
 
     TEST_STEP("Dump configuration to logs.");

commit 427e71ab55462ea6681f97ffd1d70ce365b6e731

prologue: make sure all function parameters have names
Omitting parameter names is not standard practice in C. GCC 9.3.0 emits
errors when it encounters such code.

Fixes: 5fd994e842a8 ("prologue: fix random failures because of unexpected packets")
Signed-off-by: Viacheslav Galaktionov <viacheslav.galaktionov@arknetworks.am>
Reviewed-by: Nikolai Kosovskii <Nikolai.Kosovskii@arknetworks.am>

net-drv-ts/prologue.c
@@ -104,12 +104,17 @@ cleanup:
 }
 
 static te_errno
-disable_fw_lldp(cfg_net_t *, cfg_net_node_t *, const char *,
-                cfg_oid *oid, void *)
+disable_fw_lldp(cfg_net_t *net, cfg_net_node_t *node, const char *str,
+                cfg_oid *oid, void *cookie)
 {
     const char *agent = CFG_OID_GET_INST_NAME(oid, 1);
     const char *iface = CFG_OID_GET_INST_NAME(oid, 2);
     te_errno rc;
+
+    UNUSED(net);
+    UNUSED(node);
+    UNUSED(str);
+    UNUSED(cookie);
 
     /*
      * Try to disable FW LLDP using private flag if it is present.